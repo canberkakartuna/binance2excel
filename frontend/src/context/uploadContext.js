e1d35181d838a92053fbb92b2c34ede4a4635686
Canberk Akartuna <canberkakartuna@hotmail.com>
2021-10-05 15:05:40 +0300

    If there is server 500 error, page stays at loading state fixes #2
    - Error status added to context
    - New error message added to Home

--- frontend/src/context/uploadContext.js
+++ frontend/src/context/uploadContext.js
@@ -3,10 +3,11 @@ import React, {createContext, useState} from 'react';
 export const UploadContext = createContext();
 
 export const UploadProvider = (props) => {
-  const [busy, setBusy] = useState(false);
+  const [busy, setBusy] = useState(null);
+  const [errorStatus, setErrorStatus] = useState(null);
 
   return (
-    <UploadContext.Provider value={[busy, setBusy]}>
+    <UploadContext.Provider value={{busy, setBusy, errorStatus, setErrorStatus}}>
       {props.children}
     </UploadContext.Provider>
   );
