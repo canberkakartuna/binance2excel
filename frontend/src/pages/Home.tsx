b09237aec34f74e3421f5ee7fe6e57cad5b4e849
Canberk Akartuna <canberkakartuna@hotmail.com>
2021-10-05 10:02:51 +0300

    loading text changed and styled
     -Text changed from 'Loading' to 'Your File Is Being Processed'

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,19 +1,37 @@
-import React, {useContext, Fragment, useEffect} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import UploadInput from "../components/shared/UploadInput";
 import { UploadContext } from "../context/uploadContext";
 
 const Home = () => {
   const [busy, setBusy] = useContext(UploadContext);
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    if(counter > 3) {
+      setCounter(1);
+    }
+
+    const timeout = setTimeout(() => {
+      setCounter(counter + 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [counter]);
 
   return (
-  <div className="flex flex-col h-screen justify-center items-center text-center">
+  <div className="flex flex-col h-screen justify-center text-white">
     {
       busy ? (
-        <div><h1>Uploading...</h1></div>
+        <div className="text-2xl m-auto w-96 font-bold">
+            <span>Your File Is Being Processed</span>
+            <span>{'.'.repeat(counter)}</span>
+        </div>
       ) : (
-        <div>
-          <h1 className="text-5xl text-white mb-5">Binance2Excel</h1>
-          <p className="text-2xl text-white font-light mb-5">
+        <div className="items-center text-center">
+          <h1 className="text-5xl mb-5">Binance2Excel</h1>
+          <p className="text-2xl font-light mb-5">
             This tool allows you to convert your Binance trade history into an Excel
           </p>
           <UploadInput />
